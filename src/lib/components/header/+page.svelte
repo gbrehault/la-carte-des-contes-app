bc578a32462574698091c14963e7908b3776c835
Gabriel BREHAULT <gabrielbrehault@MacBook-Pro-de-Gabriel.local>
2025-10-28 12:54:49 +0100

    add loader

---
+++ src/lib/components/header/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { page } from "$app/stores"; // optionnel, si tu veux savoir sur quelle page tu es
+</script>
+
+<header class="bg-white shadow-md px-8 py-4 flex justify-between items-center">
+  <a href="/" class="text-xl font-bold text-gray-800">La Carte des Contes</a>
+
+  <nav class="flex gap-6">
+    <a href="/" class="hover:text-blue-600">Accueil</a>
+    <a href="/chapitres" class="hover:text-blue-600">Chapitres</a>
+    <a href="/contact" class="hover:text-blue-600">Contact</a>
+  </nav>
+</header>
