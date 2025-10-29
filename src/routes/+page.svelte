886907c10b181ea33f622b419505551d70d345a9
Gabriel BREHAULT <gabrielbrehault@MacBook-Pro-de-Gabriel.local>
2025-10-29 19:50:53 +0100

    final page 3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,21 +10,25 @@
       <img src={Logo} alt="" class="w-50 h-auto fixed top-10 left-10" />
     </a>
   </div>
-  <div
-    class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_alice.png')] bg-size-[200px] bg-center bg-no-repeat absolute bottom-50 left-100 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
-  >
-    <h3 class="font-light text-3xl font-title text-center text-bg">
-      Alice au Pays <br /> des Merveilles
-    </h3>
-  </div>
-  <div
-    class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_peter.png')] bg-size-[200px] bg-center bg-no-repeat absolute top-40 right-35 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
-  >
-    <h3 class="font-light text-3xl font-title text-center text-bg">
-      Peter Pan
-    </h3>
-  </div>
-  <a href="/le-petit-chaperon-rouge">
+  <a href="alice-au-pays-des-merveilles" class="z-999">
+    <div
+      class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_alice.png')] bg-size-[200px] bg-center bg-no-repeat absolute bottom-50 left-100 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
+    >
+      <h3 class="font-light text-3xl font-title text-center text-bg">
+        Alice au Pays <br /> des Merveilles
+      </h3>
+    </div>
+  </a>
+  <a href="peter-pan" class="z-999">
+    <div
+      class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_peter.png')] bg-size-[200px] bg-center bg-no-repeat absolute top-40 right-35 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
+    >
+      <h3 class="font-light text-3xl font-title text-center text-bg">
+        Peter Pan
+      </h3>
+    </div>
+  </a>
+  <a href="le-petit-chaperon-rouge" class="z-999">
     <div
       class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_chap.png')] bg-size-[220px] bg-center bg-no-repeat absolute bottom-76 -right-2 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
     >
@@ -33,11 +37,13 @@
       </h3>
     </div></a
   >
-  <div
-    class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_barbe.png')] bg-size-[200px] bg-center bg-no-repeat absolute top-70 left-135 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
-  >
-    <h3 class="font-light text-3xl font-title text-center text-bg">
-      Barbe Bleue
-    </h3>
-  </div>
+  <a href="barbe-bleue" class="z-999">
+    <div
+      class="p-10 w-1/3 mx-auto text-center bg-[url('/assets/button_home_barbe.png')] bg-size-[200px] bg-center bg-no-repeat absolute top-70 left-135 transform -translate-x-1/2 transition-transform duration-500 hover:scale-105"
+    >
+      <h3 class="font-light text-3xl font-title text-center text-bg">
+        Barbe Bleue
+      </h3>
+    </div>
+  </a>
 </section>
